f2f6cbe4bf636965cf862c0e1f483f0045eb2a09
doulighan <doulighan@gmail.com>
2017-05-31 11:53:47 -0400

    finished user

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,5 @@
 class PicturesController < ApplicationController
+  before_action :authenticate, except: :index
 
   def index
     @pictures = Picture.all
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate, only: :show
+  
   def new
     @user = User.new
   end
